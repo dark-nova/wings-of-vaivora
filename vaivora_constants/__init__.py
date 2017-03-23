eb4f2adbe902f049b9a4b96758f0a6d1700ae042
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2017-03-23 04:49:13 -0700

    Initial commit: change constants to modules

---
+++ vaivora_constants/__init__.py
@@ -0,0 +1,19 @@
+__all__ = [ "values", \
+            "values.time.wait", \
+            "values.time.offset", \
+            "values.time.seconds", \
+            "values.words.message", \
+            "values.filenames", \
+            "values.error_codes", \
+            "command.boss", \
+            # "command.talt_tracker", \
+            # "command.reminders", \
+            "command.syntax", \
+            "regex.db", \
+            "regex.boss.status", \
+            "regex.boss.location", \
+            "regex.boss.command", \
+            "regex.format.matching", \
+            "regex.format.time"
+
+            ]
---
+++ vaivora_constants/values/time/seconds.py
@@ -0,0 +1,4 @@
+#vaivora_constants.values.time.seconds
+from datetime import timedelta
+
+in_day  = int(timedelta(days=1).seconds)
