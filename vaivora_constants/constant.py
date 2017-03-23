9fe1044683d1d71570cace7889a04ee84577eeff
CJ Wong <christopher.jay.wong@gmail.com>
2017-03-23 04:49:13 -0700

    Initial commit: change constants to modules

---
+++ vaivora_constants/constant.py
@@ -0,0 +1,50 @@
+import re
+import sys
+from datetime import datetime, timedelta
+
+# constants
+#   constant dictionary
+class TOS_CONSTANTS:
+    def __init__(self):
+        pass
+
+
+
+    def rx():
+        def vaivora():
+            def cmd():
+                def boss():
+                    
+        
+
+    def reason():
+        def invalid():
+            database            = ""
+    #   regular constants
+    con = dict()
+
+    #   regex dictionary
+    rx = dict()
+
+
+    #   error(**) related constants
+    #     error(**) constants for "command" argument
+    cmd                         = dict()
+    cmd['name']                 = "Command: Boss"
+    # cmd['talt']                 = "Command: Talt Tracker" ####TODO
+    # cmd['reminders']            = "Command: Reminders"    ####TODO
+    #     error(**) constants for "reason" argument
+
+    # reason = dict()
+    # reason['baddb'] = con['STR.REASON'] + "Bad Database"
+    # reason['unkwn'] = con['STR.REASON'] + "Unknown"
+    # reason['broad'] = con['STR.REASON'] + "Broad Command"
+    # reason['argct'] = con['STR.REASON'] + "Argument Count"
+    # reason['noanc'] = con['STR.REASON'] + "Cannot Anchor"
+    # reason['unknb'] = con['STR.REASON'] + "Unknown Boss"
+    # reason['syntx'] = con['STR.REASON'] + "Malformed Syntax"
+    # reason['quote'] = con['STR.REASON'] + "Mismatched Quotes"
+    # reason['bdmap'] = con['STR.REASON'] + "Bad Map"
+    # reason['bdtme'] = con['STR.REASON'] + "Bad Time"
+    # reason['fdbos'] = con['STR.REASON'] + "Field Boss Channel"
+
---
+++ vaivora_constants/values/filenames.py
@@ -0,0 +1,12 @@
+#vaivora_constants.values.filenames
+
+wings       = "wings-"
+txt         = ".txt"
+log         = ".log"
+
+valid_db    = wings + "valid_db"    + txt
+norepeat    = wings + "no_repeat"   + txt
+welcomed    = wings + "welcomed"    + txt
+
+file        = wings + "logger"      + log
+debug       = wings + "debug"       + log
