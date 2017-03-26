5fdccf42afb0f32bb962490b3f826622aedba674
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2017-03-26 11:31:38 -0700

    First stable push of vaivora_constants

--- vaivora_constants/__init__.py
+++ vaivora_constants/__init__.py
@@ -1,19 +1,27 @@
-__all__ = [ "values", \
+__all__ = [ #"values", \
+            #"command", \
+            #"regex", \
+            #"values.time", \
             "values.time.wait", \
             "values.time.offset", \
             "values.time.seconds", \
+            "command.syntax", \
+            # command.boss requires command.syntax
+            "command.boss", \
+            # values.words.message requires command.boss
             "values.words.message", \
             "values.filenames", \
             "values.error_codes", \
-            "command.boss", \
             # "command.talt_tracker", \
             # "command.reminders", \
-            "command.syntax", \
             "regex.db", \
             "regex.boss.status", \
             "regex.boss.location", \
             "regex.boss.command", \
             "regex.format.matching", \
             "regex.format.time"
-
             ]
+
+vaivora = [ "vaivora_constants" ] * len(__all__)
+
+modules = [ '.'.join(t) for t in zip(vaivora, __all__) ]
