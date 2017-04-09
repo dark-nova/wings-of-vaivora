92e2523051de3b004027d7ad306747300da9ecac
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2017-04-09 03:04:12 -0700

    Completed milestone 2: database and constants modules separated from bot.py

--- vaivora_constants/regex/format/time.py
+++ vaivora_constants/regex/format/time.py
@@ -2,6 +2,6 @@
 import re
 
 full    = re.compile(r'[0-2]?[0-9][:.][0-5][0-9]([ap]m?)?', re.IGNORECASE)
-am      = re.compile(r' ?pm?', re.IGNORECASE)
-pm      = re.compile(r' ?am?', re.IGNORECASE)
+am      = re.compile(r' ?am?', re.IGNORECASE)
+pm      = re.compile(r' ?pm?', re.IGNORECASE)
 delim   = re.compile(r'[:.]')
