6a13a5622e6fee34fc3a74a3b82f76f7dd454480
CJ Wong <christopher.jay.wong@gmail.com>
2017-06-20 20:54:10 -0700

    Corrected issue with stable

--- stable/vaivora_constants/fun.py
+++
@@ -1,6 +0,0 @@
-#vaivora_constants.fun
-import re
-
-ohoho       = re.compile(r'\#(oh)+', re.IGNORECASE)
-meme        = re.compile(r'vaivora[, ]*pl(ea)?[sz]e?', re.IGNORECASE)
-stab        = re.compile(r'[^spw]?(kill|edg[ey]|maim|stab|🗡|⚔).*', re.IGNORECASE)
