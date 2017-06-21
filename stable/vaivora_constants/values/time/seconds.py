cb8bd46bc9a0037adacb37f18afe991e88208c71
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2017-06-20 20:54:10 -0700

    Corrected issue with stable

--- stable/vaivora_constants/values/time/seconds.py
+++
@@ -1,4 +0,0 @@
-#vaivora_constants.values.time.seconds
-from datetime import timedelta
-
-in_day  = int(timedelta(days=1).seconds)
--- stable/vaivora_constants/values/time/wait.py
+++
@@ -1,5 +0,0 @@
-#vaivora_constants.values.time.wait
-from datetime import timedelta
-
-respawn = timedelta(hours=4)
-anchor  = timedelta(hours=3)
