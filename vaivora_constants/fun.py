223583d5274c4ae283922e9c2638fbb6a4e22132
CJ Wong <christopher.jay.wong@gmail.com>
2017-06-25 08:57:17 -0700

    Adjusted some small things.

--- vaivora_constants/fun.py
+++ vaivora_constants/fun.py
@@ -4,3 +4,7 @@ import re
 ohoho       = re.compile(r'\#(oh)+', re.IGNORECASE)
 meme        = re.compile(r'vaivora[, ]*pl(ea)?[sz]e?', re.IGNORECASE)
 stab        = re.compile(r'[^spw]?(kill|edg[ey]|maim|stab|🗡|⚔).*', re.IGNORECASE)
+stab2       = re.compile(r'.*[^s]?kill.*', re.IGNORECASE)
+stab3       = re.compile(r'.*[^lw]?edg[ey].*', re.IGNORECASE)
+stab4       = re.compile(r'.*stab[^l]*', re.IGNORECASE)
+stab5       = re.compile(r'.*maim.*', re.IGNORECASE)
