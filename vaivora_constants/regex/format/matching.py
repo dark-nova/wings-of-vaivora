9667bcd3eda195f903953e3a3384df6ba57cb962
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2017-08-23 18:11:55 -0700

    m1.8: finished settings rewrite, ready to push to public

--- vaivora_constants/regex/format/matching.py
+++
@@ -1,8 +0,0 @@
-#vaivora_constants.regex.format.matching
-import re
-
-letters     = re.compile(r'[a-z -]+', re.IGNORECASE)
-numbers     = re.compile(r'^[0-9]+$', re.IGNORECASE)
-one_number  = re.compile(r'^[0-9]{1}$')
-to_sanitize = re.compile(r'[^a-z0-9 .:$",-]', re.IGNORECASE)
-quotes      = re.compile(r'"')
