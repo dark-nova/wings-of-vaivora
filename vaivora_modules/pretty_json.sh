ad2a9c87c8797cd34dab3a3ef0e105f8d56c743b
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2018-01-09 20:36:21 -0800

    Tidied pretty_json.sh

--- vaivora_modules/pretty_json.sh
+++ vaivora_modules/pretty_json.sh
@@ -4,10 +4,12 @@ file="version.json"
 
 # modified from: https://stackoverflow.com/a/677212
 type python > /dev/null 2>&1 || exit
-# end
+# end modification
 
 [ -f "$file" ] || exit
 
 # modified from: https://stackoverflow.com/a/32228333
 out=$(cat "$file" | python -m json.tool)
+# end modification
+
 echo "$out" > "$file"
