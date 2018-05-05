bef34078c39ee6d9351f460f0b7aa49ab91aabf5
CJ Wong <christopher.jay.wong@gmail.com>
2018-05-04 19:47:27 -0700

    Add new COMMAND constant string for boss

---
+++ constants/errors/en_us.py
@@ -0,0 +1,7 @@
+CANT_DM = '**{}** is not available in Direct Messages.'
+
+TOO_FEW_ARGS = '{} Too few arguments for {}. Usage: {}'
+
+IS_INVALID_2 = '{} {} is invalid for {}.'
+
+IS_INVALID_3 = '{} {} is invalid for {} {}.'
