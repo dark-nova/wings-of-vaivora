f9cc15aa68d6253abfed52d7f51172226725c055
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2018-05-15 19:08:58 -0700

    Completely rewrote db to fit new constants and rewrite

--- vaivora_modules/db.py
+++ vaivora_modules/db.py
@@ -4,13 +4,40 @@ from importlib import import_module as im
 import vaivora_modules
 for mod in vaivora_modules.modules:
     im(mod)
-from constants.boss import en_us as lang
+from constants.boss import en_us as lang_boss
+from constants.db import en_us as lang_db
+
+
+def construct_SQL(*args):
+    """
+    :func:`construct_SQL` creates a SQLite statement.
+
+    Args:
+        args (tuple): the statements to use into one statement
+
+    Returns:
+        str: a full SQLite statement
+    """
+    return ' '.join([str(arg) for arg in args])
+
+
+def construct_filters(*args):
+    """
+    :func:`construct_filters` creates a compound SQLite filter in string form.
+
+    Args:
+        args (tuple): the filters
+
+    Returns:
+        str: a compound SQLite filter
+    """
+    return '{} {}'.format(lang_db.SQL_WHERE,
+                          ' {} '.format(lang_db.SQL_AND).join([str(arg) for arg in args]))
 
 
 class Database:
     # constants
     TIME                    =   "Time"
-    modules                 =   [ 'boss' ]
     # BOSS        = "Module: Boss"
     # REMINDERS   = "Module: Reminders"
     # TALT        = "Module: Talt Tracker"
@@ -19,7 +46,7 @@ class Database:
     # database formats
     columns                 =   dict()
     columns[TIME]           =   ('year', 'month', 'day', 'hour', 'minute')
-    columns[modules[0]]     =   ('name', 'channel', 'map', 'status', 'text_channel') + columns[TIME]
+    columns[lang_db.MOD]     =   ('name', 'channel', 'map', 'status', 'text_channel') + columns[TIME]
     # columns[modules[1]]     =   ('user', 'comment') + columns[TIME]
     # columns[modules[2]]     =   ('user', 'previous', 'current', 'valid') + columns[TIME]
     # columns[modules[3]]     =   ('user', 'role')
@@ -27,69 +54,71 @@ class Database:
     # and the database formats' types
     types                   =   dict()
     types[TIME]             =   ('real',)*5
-    types[modules[0]]       =   ('text',) + ('real',) + ('text',)*3 + types[TIME]
+    types[lang_db.MOD]       =   ('text',) + ('real',) + ('text',)*3 + types[TIME]
     # types[modules[1]]       =   ('text',)*2 + types[TIME]
     # types[modules[2]]       =   ('text',) + ('real',)*3 + types[TIME]
     # types[modules[3]]       =   ('text',)*2
 
     # zip, create, concatenate into tuple
-    dbs                     =   dict()
-    dbs[modules[0]]         =   tuple('{} {}'.format(*t) for t in 
-                                      zip(columns[modules[0]], types[modules[0]]))
-    # dbs[modules[1]]         =   tuple('{} {}'.format(*t) for t in 
-    #                                   zip(columns[modules[1]], types[modules[1]]))
-    # dbs[modules[2]]         =   tuple('{} {}'.format(*t) for t in 
-    #                                   zip(columns[modules[2]], types[modules[2]]))
-    # dbs[modules[3]]         =   tuple('{} {}'.format(*t) for t in
-    #                                   zip(columns[modules[3]], types[modules[3]]))
+    dbs = dict()
+    dbs[lang_db.MOD] = tuple('{} {}'.format(*t) for t in 
+                             zip(columns[lang_db.MOD], types[lang_db.MOD]))
+    # lang_db.MOD being solely 'boss' is a legacy design
 
-    sql_select = "select * "
-    sql_count = "select count(*) "
-    sql_from_boss = "from boss "
-    sql_and = "and "
-    sql_boss_name = "name=? "
-    sql_boss_map = "map=? "
-    sql_boss_channel = "channel=?"
-    sql_boss_default = "from boss where name=?"
-    sql_order = "order by year desc, month desc, day desc, hour desc, minute desc "
 
-    def __init__(self, db_id):
-        self.db_id          = str(db_id)
-        self.db_name        = "db/" + self.db_id + ".db"
+    def __init__(self, db_id: str):
+        """
+        :func:`__init__` initializes the db module.
+
+        Args:
+            db_id (str): the db id for the guild
+        """
+        self.db_id = db_id
+        self.db_name = '{}{}{}'.format(lang_db.DIR, self.db_id, lang_db.EXT)
         self.open_db()
-        self.invalid        = self.check_if_invalid()
+        self.invalid = self.check_if_invalid()
         if self.invalid:
             self.create_db(self.invalid)
 
 
     def open_db(self):
-        self.connect        = sqlite3.connect(self.db_name)
+        """
+        :func:`open_db` opens the database for use.
+        """
+        self.connect = sqlite3.connect(self.db_name)
         self.connect.row_factory = sqlite3.Row
-        self.cursor         = self.connect.cursor()
+        self.cursor = self.connect.cursor()
 
 
     def save_db(self):
+        """
+        :func:`save_db` saves the database.
+        """
         self.connect.commit()
         #self.connect.close()
 
 
     def get_id(self):
+        """
+        :func:`get_id` returns the database id.
+
+        Returns:
+            str: this database id
+        """
         return self.db_id
 
-    # @func:    create_db(self, list)
-    # @post:    no invalid databases
-    # @arg:
-    #   conn:
-    #           the sqlite3 connection
-    #   invalid:
-    #           invalid databases to redo (default: modules)
-    # @return:
-    #   None
-    def create_db(self, invalid=modules):
+
+    def create_db(self):
+        """
+        :func:`create_db` creates a db when none (or invalid) exists.
+
+        Returns:
+            None
+        """
         #self.open_db()
         for invalid_module in invalid:
             self.cursor.execute('drop table if exists {}'.format(invalid_module))
-            self.cursor.execute('create table {}({})'.format(invalid_module, ','.join(self.dbs[invalid_module])))
+            self.cursor.execute('create table {}({})'.format(invalid_module, ','.join(self.dbs[lang_db.MOD])))
         # else:
         #     for module in modules:
         #         self.cursor.execute('create table {}({})'.format((module, ','.join(dbs[module]),)))
@@ -98,117 +127,135 @@ class Database:
         return
 
 
-    # @func:    check_if_invalid(self)
-    # @arg:
-    #   self
-    # @return:
-    #   invalid:
-    #           a list containing invalid database module names (str) or None
-    def check_if_invalid(self):
+    def check_if_valid(self):
+        """
+        :func:`check_if_valid` checks if the database is valid. Since only one module uses db, this will be one check only.
+
+        Returns:
+            bool: True if valid; False otherwise
+        """
         #self.open_db()
-        invalid = []
-        for module in self.modules:
-            try:
-                self.cursor.execute('select * from ' + module)
-            except:
-                invalid.append(module)
-                continue
-            r = self.cursor.fetchone()
-            if not r:
-                continue # it's empty. probably works.
-            if sorted(tuple(r.keys())) != sorted(self.columns[module]):
-                invalid.append(module)
-                continue
-        self.save_db()
-        return invalid
+        try:
+            self.cursor.execute(construct_SQL(lang_db.SQL_SELECT, lang_db.SQL_FROM_BOSS))
+        except:
+            return False
+
+        r = self.cursor.fetchone()
+        if not r:
+            return True
+
+        if sorted(tuple(r.keys())) != sorted(self.columns[lang_db.MOD]):
+            return False
+        else:
+            return True
 
 
-    # @func:    check_db_boss(self, list)
-    # @arg:
-    #   self
-    #   bosses:
-    #           list containing bosses to check (default: vaivora_modules.boss.bosses)
-    # @return:
-    #   db_record:
-    #           a list containing records or None
-    def check_db_boss(self, bosses=vaivora_modules.boss.bosses, channel=0):
+    def check_db_boss(self, bosses=lang_boss.ALL_BOSSES, channel=0):
+        """
+        :func:`check_db_boss` checks the database for the conditions in argument
+
+        Args:
+            bosses (list): (default: lang_boss.ALL_BOSSES) the bosses to check
+            channel (int): (default: 0) channels to filter
+
+        Returns:
+            list: records or list of None
+        """
         #self.open_db()
-        db_record = list()
+        db_record = []
         for boss in bosses:
             if channel:
-                self.cursor.execute((self.sql_select+self.sql_boss_world), (boss, channel,))
+                self.cursor.execute(construct_SQL(lang_db.SQL_SELECT, lang_db.SQL_FROM_BOSS,
+                                                  construct_filters(lang_db.SQL_WHERE_NAME,
+                                                                    lang_db.SQL_WHERE_CHANNEL)),
+                                    (boss, channel,))
             else:
-                self.cursor.execute((self.sql_select+self.sql_boss_default), (boss,))
+                self.cursor.execute(construct_SQL(lang_db.SQL_SELECT, lang_db.SQL_FROM_BOSS,
+                                                  construct_filters(lang_db.SQL_WHERE_NAME)),
+                                    (boss,))
+
             records = self.cursor.fetchall()
             for record in records:
                 db_record.append(tuple(record))
+
         self.save_db()
-        db_record   =   self.sort_db(db_record)
-        # return a list of records
-        return db_record
+        return self.sort_db_record(db_record)
 
 
-    def sort_db(self, rec_db):
-        return sorted(rec_db, key=itemgetter(5,6,7,8,9), reverse=True)
+    def sort_db_record(self, db_record):
+        """
+        :func:`sort_db_record` sorts the db records by chronological order.
+
+        Args:
+            db_record (list): the records to sort
+
+        """
+        return sorted(db, key=itemgetter(5,6,7,8,9), reverse=True)
 
 
-
-    # @func:    update_db_boss(sqlite3.connect.cursor, dict)
-    # @arg:
-    #   c:              the sqlite3 connection cursor
-    #   boss_dict:      message_args from on_message(*)
-    # @return:
-    #   True if successful, False otherwise
     def update_db_boss(self, boss_dict):
+        """
+        :func:`update_db_boss` updates the record with a new entry.
+
+        Args:
+            boss_dict (dict): the boss dictionary containing the new record
+
+        Returns:
+            bool: True if successful; False otherwise
+        """
         contents = []
         #self.open_db()
 
-        # handle world bosses
+        # handle channels
         if boss_dict['channel']:
-            sel_statement   =   self.sql_select+self.sql_boss_world
-            sql_condition   =   (boss_dict['boss'], boss_dict['channel'])
-        
+            sel_statement = construct_SQL(lang_db.SQL_SELECT, lang_db.SQL_FROM_BOSS,
+                                          construct_filters(lang_db.SQL_WHERE_NAME,
+                                                            lang_db.SQL_WHERE_CHANNEL))
+            sql_condition = (boss_dict['boss'], boss_dict['channel'])
         # handle everything else
         else:
-            sel_statement   =   self.sql_select+self.sql_boss_default
-            sql_condition   =   (boss_dict['boss'],)
+            sel_statement = construct_SQL(lang_db.SQL_SELECT, lang_db.SQL_FROM_BOSS,
+                                          construct_filters(lang_db.SQL_WHERE_NAME))
+            sql_condition = (boss_dict['boss'],)
 
         self.cursor.execute(sel_statement, sql_condition)
         contents.extend(self.cursor.fetchall())
 
-        if contents and (int(contents[0][5]) == boss_dict['year'] and \
-                         int(contents[0][6]) == boss_dict['month'] and \
-                         int(contents[0][7]) == boss_dict['day'] and \
+        if contents and (int(contents[0][5]) == boss_dict['year'] and
+                         int(contents[0][6]) == boss_dict['month'] and
+                         int(contents[0][7]) == boss_dict['day'] and
                          int(contents[0][8]) >  boss_dict['hour']):
             return False
-        elif contents and (int(contents[0][5]) <= boss_dict['year'] or \
-                           int(contents[0][6]) <= boss_dict['month'] or \
-                           int(contents[0][7]) <= boss_dict['day'] or \
+        elif contents and (int(contents[0][5]) <= boss_dict['year'] or
+                           int(contents[0][6]) <= boss_dict['month'] or
+                           int(contents[0][7]) <= boss_dict['day'] or
                            int(contents[0][8]) <= boss_dict['hour']):
 
-            if boss_dict['boss'] in vaivora_modules.boss.bosses_world:
+            if boss_dict['channel']:
                 self.rm_entry_db_boss(boss_list=[boss_dict['boss'],], boss_ch=boss_dict['channel'])
             else:
                 self.rm_entry_db_boss(boss_list=[boss_dict['boss'],])
 
+        try:
+            # boss database structure
+            self.cursor.execute(lang_db.SQL_UPDATE,
+                                (str(boss_dict['boss']),
+                                 int(boss_dict['channel']),
+                                 str(boss_dict['map']),
+                                 str(boss_dict['status']),
+                                 str(boss_dict['text_channel']),
+                                 int(boss_dict['year']),
+                                 int(boss_dict['month']),
+                                 int(boss_dict['day']),
+                                 int(boss_dict['hour']),
+                                 int(boss_dict['mins'])))
+            self.save_db()
+            return True
+        except:
+            return False
 
-        #try: # boss database structure
-        self.cursor.execute("insert into boss values (?,?,?,?,?,?,?,?,?,?)", \
-                            (str(boss_dict['boss'           ]), \
-                             int(boss_dict['channel'        ]), \
-                             str(boss_dict['map'            ]), \
-                             str(boss_dict['status'         ]), \
-                             str(boss_dict['text_channel'   ]), \
-                             int(boss_dict['year'           ]), \
-                             int(boss_dict['month'          ]), \
-                             int(boss_dict['day'            ]), \
-                             int(boss_dict['hour'           ]), \
-                             int(boss_dict['mins'           ])))
-        self.save_db()
-        return True
 
-
-    def rm_entry_db_boss(self, boss_list=vaivora_modules.boss.bosses, boss_ch=0, boss_map=None):
+    def rm_entry_db_boss(self, boss_list=lang_boss.ALL_BOSSES, boss_ch=0, boss_map=None):
         """
         :func:`rm_entry_db_boss` removes records based on the conditions supplied.
 
@@ -221,36 +268,43 @@ class Database:
             list: a list containing the records that were removed
         """
         #self.open_db()
-        rec_count = 0
+        records = []
         if not boss_list:
-            boss_list = lang.ALL_BOSSES
+            boss_list = lang_boss.ALL_BOSSES
+
         for boss in boss_list:
+            # channel is provided
             if boss_ch:
-                sql_statement = self.sql_boss_world
+                sql_filters = construct_filters(lang_db.SQL_WHERE_NAME,
+                                                lang_db.SQL_WHERE_CHANNEL)
                 sql_condition = (boss, boss_ch)
-            elif boss_map and boss_map is not None:
-                sql_statement = self.sql_boss_field
+            # map is provided
+            elif boss_map is not None:
+                sql_filters = construct_filters(lang_db.SQL_WHERE_NAME,
+                                                lang_db.SQL_WHERE_MAP)
                 sql_condition = (boss, boss_map)
-            # handle all others, generally field bosses
+            # only name                
             else:
-                sql_statement = self.sql_boss_default
+                sql_filters = construct_filters(lang_db.SQL_WHERE_NAME)
                 sql_condition = (boss,)
 
             # process counting
-            ct_statement    = "select count(*) " + sql_statement
-            del_statement   = "delete " + sql_statement                
-            self.cursor.execute(ct_statement, sql_condition)
-            result  = self.cursor.fetchone()
+            sel_statement = construct_SQL(lang_db.SQL_SELECT, lang_db.SQL_FROM_BOSS, sql_filters)
+            del_statement = construct_SQL(lang_db.SQL_DELETE, sql_filters)
+
+            self.cursor.execute(sel_statement, sql_condition)
+            results = self.cursor.fetchall()
+
             # no records to delete
-            if result[0] == 0:
+            if len(results) == 0:
                 continue
-            # records to delete
-            else:
-                try:
-                    self.cursor.execute(del_statement, sql_condition)
-                    self.save_db()
-                    rec_count   +=  int(result[0])
-                except: # in case of sqlite3 exceptions
-                    continue
+
+            try:
+                self.cursor.execute(del_statement, sql_condition)
+                self.save_db()
+                records.append(boss) # guaranteed to be only one entry as per this loop
+            except: # in case of sqlite3 exceptions
+                continue
+
         self.save_db()
-        return rec_count # return an implicit bool for how many were deleted
+        return records # return an implicit bool for how many were deleted
