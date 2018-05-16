d136e0866fc4c3507cb443a722a3be0d14299190
CJ Wong <christopher.jay.wong@gmail.com>
2018-05-15 19:08:37 -0700

    Commented out a constant that might not be used at all

---
+++ constants/db/en_us.py
@@ -0,0 +1,17 @@
+SQL_SELECT = 'select *'
+#SQL_SELECT_COUNT = 'select count(*)'
+SQL_DELETE = 'delete'
+SQL_AND = 'and'
+SQL_WHERE = 'where'
+SQL_FROM_BOSS = 'from boss'
+SQL_WHERE_NAME = 'name=?'
+SQL_WHERE_MAP = 'map=?'
+SQL_WHERE_CHANNEL = 'channel=?'
+SQL_FROM_BOSS_DEFAULT = 'from boss where name=?'
+SQL_ORDER = 'order by year desc, month desc, day desc, hour desc, minute desc'
+
+SQL_UPDATE = 'insert into boss values (?,?,?,?,?,?,?,?,?,?)'
+
+MOD = 'boss'
+DIR = 'db/'
+EXT = '.db'
