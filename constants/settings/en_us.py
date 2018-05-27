41e653b32c1b7e33a5e9c6177319ffb84e8e2104
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2018-05-27 13:20:12 -0700

    Created new constants file for settings module

---
+++ constants/settings/en_us.py
@@ -0,0 +1,179 @@
+import re
+
+MODULE_NAME = 'settings'
+COMMAND = '$' + MODULE_NAME
+
+HELP = []
+HELP.append(
+"""
+```
+Usage:
+    $settings <setting> talt <talt-value> [<talt-unit>] ([<@mention>] | guild)
+    $settings <setting> (<role> [<@mention>] | <channel> [<#channel>])
+    $settings <validate> [<@mention>]
+    $settings <role-change> <@mention>
+    $settings help
+
+Examples:
+    $settings add talt 12
+        Means: Add 12 Talt to your guild record.
+        Note: If not "Authorized" or "Member", this is only temporary. It must be validated.
+
+    $settings set talt 12
+        Means: Sets your guild record of Talt to 12. Not the same as adding.
+
+    $settings add talt 240 points
+        Means: The same as `$settings add talt 12`, with optional unit.
+
+    $settings add talt 12 @person
+        Means: Add 12 Talt to @person's guild record.
+        You must be "Authorized" to do this.
+
+    $settings set channel management #management
+        Means: Sets this channel to "Management".
+        Note: this disables Management in all channels not marked as "Management".
+
+    $settings set role authorized @person
+        Means: Sets @person to be "Authorized".
+        You must be "Authorized" to do this.
+        Note: guild owners are considered "Super-Authorized" and must set this up first.
+
+    $settings promote @person
+        Means: Raises @person's authorization by one level.
+        Note: Maximum authorization is "Authorized".
+
+    $settings validate @person
+        Means: Validate any 'temporary' Talt records for @person.
+        You can omit mention; this is the same as validating everyone at once.
+```
+""")
+
+HELP.append(
+"""
+```
+Options:
+    <setting>
+        This can be "add", "set", "remove", or "get". Manipulates records.
+        Options:
+            "add" can only be used for Talt-related subcommands. Increments relative to user's base.
+            "set" can be used for all associated subcommands. Sets Talt, Role, and Channel.
+            "remove" can be used for all associated subcommands. Removes Talt contribution, Roles from users mentioned, etc.
+            "get" can be used for all associated subcommands. Retrieves Talt contribution, highest Role from users mentioned, etc.
+        Note: "Super-Authorized" will only be shown as "Authorized".
+
+    <validate>
+        Validates or invalidates records to save.
+        Options:
+            "validate" affirms temporary changes made by non-Authorized members.
+            "invalidate" removes temporary changes as if they never happened.
+
+    <role-change>
+        Changes a Discord role/member's Role*. Shortcut for <setting>.
+        Hierarchy: none < Member < Authorized (< Super-Authorized)
+        Options:
+            "promote": increases the mentioned Discord role/member's Role by 1
+            "demote": lowers the Role by 1
+        Cannot "promote" past "Authorized"; and cannot "demote" under none.
+        Note: Cannot set the "Boss" Role using this shortcut command.
+
+* Discord roles are different from Wings of Vaivora's Roles.
+```
+""")
+
+HELP.append(
+"""
+```
+Options (continued):
+    talt
+        This <subcommand> can only be "talt". Allows manipulation of Talt guild records.
+
+    <talt-value>
+        A number. The amount to use for `talt`. 20 points = 1 talt
+
+    [<talt-unit>]
+        (optional) Sets the unit to use. 20 points = 1 Talt. By default, Talt will be used if omitted.
+        Options:
+            "points": shown in-game
+            "talt": the default
+        Note: the Guild UI in-game always uses points.
+
+    guild
+        A special subcommand target for <setting> `talt`. Cannot use "add" or "remove".
+        <setting> "get" prints how many points (and Talt) the guild has.
+        <setting> "set" allows you to set the current points.
+        Missing points (unlisted contributions) will be stored in a hidden variable, for consistency.
+
+    <@mention>
+        (optional for <validate>) A Discord member. You must use the mention format for this.
+
+    [<#channel>]
+        (optional) A Discord channel. You must use the channel link format for this.
+        Both you and Wings of Vaivora must be able to see the channel.
+        If omitted in the <setting> command, the current channel will be assumed.
+
+    help
+        Prints this page.
+```
+""")
+
+ACKNOWLEDGED = "Thank you! Your command has been acknowledged and recorded.\n"
+
+MSG_HELP = "Please run `" + COMMAND + " help` for syntax."
+
+CMD_ARG_SETTING = '<setting>'
+CMD_ARG_VALIDATE = '<validate>'
+CMD_ARG_ROLECHANGE = '<role-change>'
+
+CMD_ARG_SETTING_TALT = 'Talt'
+CMD_ARG_SETTING_TALT_UNIT = 'Talt'
+CMD_ARG_SETTING_TALT_POINTS = 'Points'
+CMD_ARG_SETTING_CHANNEL = 'Channel'
+CMD_ARG_SETTING_CHANNEL_MGMT = 'Management'
+CMD_ARG_SETTING_CHANNEL_BOSS = 'Boss'
+CMD_ARG_SETTING_ROLE = 'Role'
+
+CMD_ARG_VALIDATE_VAL = 'validate'
+CMD_ARG_VALIDATE_INV = 'invalidate'
+
+CMD_ARG_ROLECHANGE_PR = 'promote'
+CMD_ARG_ROLECHANGE_DE = 'demote'
+
+### DO NOT CHANGE/TRANSLATE THIS SECTION BELOW ###
+
+# Guild Levels   1  2   3    4    5    6    7     8     9    10    11     12     13     14     15      16      17      18       19       20
+TALT_LEVEL = [0, 0, 0, 50, 125, 250, 450, 886, 1598, 2907, 4786, 7483, 11353, 16907, 24876, 52726, 160712, 345531, 742891, 1597216, 3434015]
+# TALT_LEVEL.append(0)
+# TALT_LEVEL.append(0)            # 1
+# TALT_LEVEL.append(50)           # 2
+# TALT_LEVEL.append(125)          # 3
+# TALT_LEVEL.append(250)          # 4
+# TALT_LEVEL.append(450)          # 5
+# TALT_LEVEL.append(886)          # 6
+# TALT_LEVEL.append(1598)         # 7
+# TALT_LEVEL.append(2907)         # 8
+# TALT_LEVEL.append(4786)         # 9
+# TALT_LEVEL.append(7483)         # 10
+# TALT_LEVEL.append(11353)        # 11
+# TALT_LEVEL.append(16907)        # 12
+# TALT_LEVEL.append(24876)        # 13
+# TALT_LEVEL.append(36313)        # 14
+# TALT_LEVEL.append(52726)        # 15
+# TALT_LEVEL.append(160712)       # 16
+# TALT_LEVEL.append(345531)       # 17
+# TALT_LEVEL.append(742891)       # 18
+# TALT_LEVEL.append(1597216)      # 19
+# TALT_LEVEL.append(3434015)      # 20
+
+ROLE_BOSS = 'boss'
+ROLE_NONE = 'none'
+ROLE_MEMBER = 'member'
+ROLE_AUTH = 'authorized'
+ROLE_SUPER_AUTH = 'super authorized'
+
+# Roles by value      0            1          2                3
+ROLE_LEVEL = [ROLE_NONE, ROLE_MEMBER, ROLE_AUTH, ROLE_SUPER_AUTH]
+
+UTYPE_USER = 'users'
+UTYPE_GROUP = 'group'
+
+### DO NOT CHANGE/TRANSLATE THIS SECTION ABOVE ###
