77699da25a7a6af5068f74fc3600354c63dc4e84
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2019-02-05 03:18:07 +0000

    Fix asyncio issues for boss and db

--- vaivora_modules/db.py
+++ vaivora_modules/db.py
@@ -66,25 +66,6 @@ class Database:
         """
         self.db_id = db_id
         self.db_name = '{}{}{}'.format(lang_db.DIR, self.db_id, lang_db.EXT)
-        await self.open_db()
-        if not await self.check_if_valid():
-            await self.create_db()
-
-
-    async def open_db(self):
-        """
-        :func:`open_db` opens the database for use.
-        """
-        self.connect = await aiosqlite.connect(self.db_name)
-        self.connect.row_factory = aiosqlite.Row
-        self.cursor = await self.connect.cursor()
-
-
-    async def save_db(self):
-        """
-        :func:`save_db` saves the database.
-        """
-        self.connect.commit()
 
 
     def get_id(self):
@@ -101,37 +82,40 @@ class Database:
         """
         :func:`create_db` creates a db when none (or invalid) exists.
         """
-        #self.open_db()
-        await self.cursor.execute('drop table if exists {}'.format(lang_db.MOD))
-        await self.cursor.execute('create table {}({})'.format(lang_db.MOD, ','.join(self.dbs[lang_db.MOD])))
-        # else:
-        #     for module in modules:
-        #         self.cursor.execute('create table {}({})'.format((module, ','.join(dbs[module]),)))
-        await self.save_db()
+        async with aiosqlite.connect(self.db_name) as _db:
+            await _db.execute('drop table if exists {}'.format(lang_db.MOD))
+            await _db.execute('create table {}({})'.format(lang_db.MOD, ','.join(self.dbs[lang_db.MOD])))
+            await _db.commit()
         return
 
 
     async def check_if_valid(self):
         """
-        :func:`check_if_valid` checks if the database is valid. Since only one module uses db, this will be one check only.
+        :func:`check_if_valid` checks if the database is valid.
 
         Returns:
             bool: True if valid; False otherwise
         """
-        #self.open_db()
-        try:
-            await self.cursor.execute(construct_SQL(lang_db.SQL_SELECT, lang_db.SQL_FROM_BOSS))
-        except:
-            return False
+        async with aiosqlite.connect(self.db_name) as _db:
+            _db.row_factory = aiosqlite.Row
+            try:
+                cursor = await _db.execute(construct_SQL(lang_db.SQL_SELECT, lang_db.SQL_FROM_BOSS))
+            except:
+                await cursor.close()
+                await self.create_db()
+                return False
 
-        r = await self.cursor.fetchone()
-        if not r:
-            return True
+            r = await cursor.fetchone()
+            if not r:
+                await cursor.close()
+                return True
 
-        if sorted(tuple(r.keys())) != sorted(self.columns[lang_db.MOD]):
-            return False
-        else:
-            return True
+            if sorted(tuple(r.keys())) != sorted(self.columns[lang_db.MOD]):
+                await cursor.close()
+                return False
+            
+            await cursor.close()
+        return True
 
 
     async def check_db_boss(self, bosses=lang_boss.ALL_BOSSES, channel=0):
@@ -145,25 +129,26 @@ class Database:
         Returns:
             list: records or list of None
         """
-        #self.open_db()
         db_record = []
-        for boss in bosses:
-            if channel:
-                await self.cursor.execute(construct_SQL(lang_db.SQL_SELECT, lang_db.SQL_FROM_BOSS,
-                                                  construct_filters(lang_db.SQL_WHERE_NAME,
-                                                                    lang_db.SQL_WHERE_CHANNEL)),
-                                    (boss, channel,))
-            else:
-                await self.cursor.execute(construct_SQL(lang_db.SQL_SELECT, lang_db.SQL_FROM_BOSS,
-                                                  construct_filters(lang_db.SQL_WHERE_NAME)),
-                                    (boss,))
+        async with aiosqlite.connect(self.db_name) as _db:
+            _db.row_factory = aiosqlite.Row
+            for boss in bosses:
+                if channel:
+                    cursor = await _db.execute(construct_SQL(lang_db.SQL_SELECT, lang_db.SQL_FROM_BOSS,
+                                                             construct_filters(lang_db.SQL_WHERE_NAME,
+                                                                        lang_db.SQL_WHERE_CHANNEL)),
+                                               (boss, channel,))
+                else:
+                    cursor = await _db.execute(construct_SQL(lang_db.SQL_SELECT, lang_db.SQL_FROM_BOSS,
+                                                             construct_filters(lang_db.SQL_WHERE_NAME)),
+                                              (boss,))
 
-            records = await self.cursor.fetchall()
-            for record in records:
-                db_record.append(tuple(record))
-
-        await self.save_db()
-        return self.sort_db_record(db_record)
+                records = await cursor.fetchall()
+                for record in records:
+                    db_record.append(tuple(record))
+            
+            await cursor.close()
+        return await self.sort_db_record(db_record)
 
 
     async def sort_db_record(self, db_record):
@@ -188,56 +173,62 @@ class Database:
             bool: True if successful; False otherwise
         """
         contents = []
-        #self.open_db()
+
+        boss_name = boss_dict[lang_db.COL_BOSS_NAME]
+        boss_channel = boss_dict[lang_db.COL_BOSS_CHANNEL]
 
         # handle channels
-        if boss_dict[lang_db.COL_BOSS_CHANNEL]:
+        if boss_channel:
             sel_statement = construct_SQL(lang_db.SQL_SELECT, lang_db.SQL_FROM_BOSS,
                                           construct_filters(lang_db.SQL_WHERE_NAME,
                                                             lang_db.SQL_WHERE_CHANNEL))
-            sql_condition = (boss_dict[lang_db.COL_BOSS_NAME], boss_dict[lang_db.COL_BOSS_CHANNEL])
+            sql_condition = (boss_name, boss_channel)
         # handle everything else
         else:
             sel_statement = construct_SQL(lang_db.SQL_SELECT, lang_db.SQL_FROM_BOSS,
                                           construct_filters(lang_db.SQL_WHERE_NAME))
-            sql_condition = (boss_dict[lang_db.COL_BOSS_NAME],)
+            sql_condition = (boss_name,)
 
-        await self.cursor.execute(sel_statement, sql_condition)
-        contents.extend(await self.cursor.fetchall())
+        async with aiosqlite.connect(self.db_name) as _db:
+            _db.row_factory = aiosqlite.Row
 
-        if contents and (int(contents[0][5]) == boss_dict[lang_db.COL_TIME_YEAR] and
-                         int(contents[0][6]) == boss_dict[lang_db.COL_TIME_MONTH] and
-                         int(contents[0][7]) == boss_dict[lang_db.COL_TIME_DAY] and
-                         int(contents[0][8]) >  boss_dict[lang_db.COL_TIME_HOUR]):
-            return False
-        elif contents and (int(contents[0][5]) <= boss_dict[lang_db.COL_TIME_YEAR] or
-                           int(contents[0][6]) <= boss_dict[lang_db.COL_TIME_MONTH] or
-                           int(contents[0][7]) <= boss_dict[lang_db.COL_TIME_DAY] or
-                           int(contents[0][8]) <= boss_dict[lang_db.COL_TIME_HOUR]):
+            cursor = await _db.execute(sel_statement, sql_condition)
+            contents.extend(await cursor.fetchall())
 
-            if boss_dict[lang_db.COL_BOSS_CHANNEL]:
-                self.rm_entry_db_boss(boss_list=[boss_dict[lang_db.COL_BOSS_NAME],],
-                                      boss_ch=boss_dict[lang_db.COL_BOSS_CHANNEL])
-            else:
-                self.rm_entry_db_boss(boss_list=[boss_dict[lang_db.COL_BOSS_NAME],])
+            if contents and (int(contents[0][5]) == boss_dict[lang_db.COL_TIME_YEAR] and
+                             int(contents[0][6]) == boss_dict[lang_db.COL_TIME_MONTH] and
+                             int(contents[0][7]) == boss_dict[lang_db.COL_TIME_DAY] and
+                             int(contents[0][8]) >  boss_dict[lang_db.COL_TIME_HOUR]):
+                cursor.close()
+                return False
+            elif contents and (int(contents[0][5]) <= boss_dict[lang_db.COL_TIME_YEAR] or
+                               int(contents[0][6]) <= boss_dict[lang_db.COL_TIME_MONTH] or
+                               int(contents[0][7]) <= boss_dict[lang_db.COL_TIME_DAY] or
+                               int(contents[0][8]) <= boss_dict[lang_db.COL_TIME_HOUR]):
 
-        try:
-            # boss database structure
-            await self.cursor.execute(lang_db.SQL_UPDATE,
-                                (str(boss_dict[lang_db.COL_BOSS_NAME]),
-                                 int(boss_dict[lang_db.COL_BOSS_CHANNEL]),
-                                 str(boss_dict[lang_db.COL_BOSS_MAP]),
-                                 str(boss_dict[lang_db.COL_BOSS_STATUS]),
-                                 str(boss_dict[lang_db.COL_BOSS_TXT_CHANNEL]),
-                                 int(boss_dict[lang_db.COL_TIME_YEAR]),
-                                 int(boss_dict[lang_db.COL_TIME_MONTH]),
-                                 int(boss_dict[lang_db.COL_TIME_DAY]),
-                                 int(boss_dict[lang_db.COL_TIME_HOUR]),
-                                 int(boss_dict[lang_db.COL_TIME_MINUTE])))
-            await self.save_db()
-            return True
-        except:
-            return False
+                if boss_channel:
+                    await self.rm_entry_db_boss(boss_list=[boss_name,],
+                                                boss_ch=boss_channel)
+                else:
+                    await self.rm_entry_db_boss(boss_list=[boss_name,])
+
+            try:
+                # boss database structure
+                await _db.execute(lang_db.SQL_UPDATE,
+                                  (str(boss_name),
+                                   int(boss_channel),
+                                   str(boss_dict[lang_db.COL_BOSS_MAP]),
+                                   str(boss_dict[lang_db.COL_BOSS_STATUS]),
+                                   str(boss_dict[lang_db.COL_BOSS_TXT_CHANNEL]),
+                                   int(boss_dict[lang_db.COL_TIME_YEAR]),
+                                   int(boss_dict[lang_db.COL_TIME_MONTH]),
+                                   int(boss_dict[lang_db.COL_TIME_DAY]),
+                                   int(boss_dict[lang_db.COL_TIME_HOUR]),
+                                   int(boss_dict[lang_db.COL_TIME_MINUTE])))
+                await _db.commit()
+                return True
+            except:
+                return False
 
 
     async def rm_entry_db_boss(self, boss_list=lang_boss.ALL_BOSSES, boss_ch=0, boss_map=None):
@@ -252,47 +243,47 @@ class Database:
         Returns:
             list: a list containing the records that were removed
         """
-        #self.open_db()
         records = []
         if not boss_list:
             boss_list = lang_boss.ALL_BOSSES
 
-        for boss in boss_list:
-            # channel is provided
-            if boss_ch:
-                sql_filters = construct_filters(lang_db.SQL_WHERE_NAME,
-                                                lang_db.SQL_WHERE_CHANNEL)
-                sql_condition = (boss, boss_ch)
-            # map is provided
-            elif boss_map is not None:
-                sql_filters = construct_filters(lang_db.SQL_WHERE_NAME,
-                                                lang_db.SQL_WHERE_MAP)
-                sql_condition = (boss, boss_map)
-            # only name                
-            else:
-                sql_filters = construct_filters(lang_db.SQL_WHERE_NAME)
-                sql_condition = (boss,)
+        async with aiosqlite.connect(self.db_name) as _db:
+            _db.row_factory = aiosqlite.Row
 
-            # process counting
-            sel_statement = construct_SQL(lang_db.SQL_SELECT, lang_db.SQL_FROM_BOSS, sql_filters)
-            del_statement = construct_SQL(lang_db.SQL_DELETE, lang_db.SQL_FROM_BOSS, sql_filters)
+            for boss in boss_list:
+                # channel is provided
+                if boss_ch:
+                    sql_filters = construct_filters(lang_db.SQL_WHERE_NAME,
+                                                    lang_db.SQL_WHERE_CHANNEL)
+                    sql_condition = (boss, boss_ch)
+                # map is provided
+                elif boss_map is not None:
+                    sql_filters = construct_filters(lang_db.SQL_WHERE_NAME,
+                                                    lang_db.SQL_WHERE_MAP)
+                    sql_condition = (boss, boss_map)
+                # only name                
+                else:
+                    sql_filters = construct_filters(lang_db.SQL_WHERE_NAME)
+                    sql_condition = (boss,)
 
-            await self.cursor.execute(sel_statement, sql_condition)
-            results = await self.cursor.fetchall()
+                # process counting
+                sel_statement = construct_SQL(lang_db.SQL_SELECT, lang_db.SQL_FROM_BOSS, sql_filters)
+                del_statement = construct_SQL(lang_db.SQL_DELETE, lang_db.SQL_FROM_BOSS, sql_filters)
 
-            await self.save_db()
+                cursor = await _db.execute(sel_statement, sql_condition)
+                results = await cursor.fetchall()
 
-            # no records to delete
-            if len(results) == 0:
-                continue
+                # no records to delete
+                if len(results) == 0:
+                    continue
 
-            try:
-                await self.cursor.execute(del_statement, sql_condition)
-                await self.save_db()
-                records.append(boss) # guaranteed to be only one entry as per this loop
-            except Exception as e: # in case of sqlite3 exceptions
-                print(self.db_id, e)
-                continue
+                try:
+                    await cursor.execute(del_statement, sql_condition)
+                    await _db.commit()
+                    records.append(boss) # guaranteed to be only one entry as per this loop
+                except Exception as e: # in case of sqlite3 exceptions
+                    print(self.db_id, e)
+                    continue
+            await cursor.close()
 
-        await self.save_db()
         return records # return an implicit bool for how many were deleted
