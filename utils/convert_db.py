a103881c622f84bf35bed81436bec27d3a4c5fb5
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2019-02-11 18:53:02 -0800

    Complete db modification util

--- utils/convert_db.py
+++ utils/convert_db.py
@@ -38,6 +38,9 @@ def update_db(db_dir):
     :func:`update_db` amends databases given a `db_dir`.
     This assumes the existing databases, if they exist at all, are valid.
 
+    Args:
+        db_dir (str): the location of the database files
+
     Returns:
         list: len of 0 for no errors, +1 for each additional error
     """
@@ -49,14 +52,121 @@ def update_db(db_dir):
                 db = os.path.join(os.path.abspath(db_dir), db)
                 conn = sqlite3.connect(db)
                 cursor = conn.cursor()
-                cursor.execute('create table roles(role text, mention text)')
-                cursor.execute('create table channels(type text, channel text)')
-                cursor.execute('create table contribution(userid text, points real)')
+                cursor.execute('drop table if exists permissions')
+                cursor.execute('drop table if exists reminders')
+                cursor.execute('drop table if exists talt')
+                cursor.execute(
+                    'create table offset(hours integer)')
+                cursor.execute(
+                    'create table guild(level integer, points integer)')
+                cursor.execute(
+                    'create table roles(role text, mention text)')
+                cursor.execute(
+                    'create table channels(type text, channel text)')
+                cursor.execute(
+                    'create table contribution(userid text, points integer)')
                 conn.commit()
                 conn.close()
-            except:
+            except Exception as e:
+                print('Exception handled:', e)
                 errs.append(db)
+
+    return errs
+
+
+def transfer_data(db_dir, ss_dir, skips=None):
+    """
+    :func:`transfer_data` moves data existing from files in `ss_dir` to `db_dir`.
+
+    Args:
+        db_dir (str): the location of the database files
+        ss_dir (str): the location of the existing server settings files
+        skips: (default: None) the invalid databases to skip
+
+    Returns:
+        list: len of 0 of no errors, +1 for each additional error
+    """
+    errs = []
+
+    for db in os.listdir(db_dir):
+        if db.endswith('.db'):
+            ss = db.rstrip('.db') + '.json'
+            ss = os.path.join(os.path.abspath(ss_dir), ss)
+            db = os.path.join(os.path.abspath(db_dir), db)
+            if skips and db in skips:
+                errs.append(db)
+                continue
+            if not os.path.isfile(ss):
+                errs.append(ss)
+                continue
+
+            try:
+                with open(ss, 'r') as ssf:
+                    ssjson = json.load(ssf)
+            except json.JSONDecodeError as e:
+                print('Couldn\'t read JSON!', e)
+                errs.append(ss)
+                continue
+
+            conn = sqlite3.connect(db)
+            cursor = conn.cursor()
+
+
+            # add users back to their assigned roles
+            for role, user in ssjson['users'].items():
+                for _u in user:
+                    cursor.execute('insert into roles values(?, ?)',
+                                   (role, _u))
+
+            conn.commit()
+
+            # add groups back to their assigned roles
+            for role, group in ssjson['group'].items():
+                for _g in group:
+                    cursor.execute('insert into roles values(?, ?)',
+                                   (role, _g))
+
+            conn.commit()
+
+            # add only groups back to boss role
+            for role, group in ssjson['role'].items():
+                for _g in group:
+                    if '@' in _g:
+                        continue
+                    cursor.execute('insert into roles values(?, ?)',
+                                   (role, _g))
+
+            conn.commit()
+
+            # add channels
+            for ch_type, channels in ssjson['channel'].items():
+                for channel in channels:
+                    cursor.execute('insert into channels values(?, ?)',
+                                   (ch_type, channel))
+
+            conn.commit()
+
+            # add contribution points to guild
+            for user, points in ssjson['talt'].items():
+                if type(points) is not int:
+                    points = int(points)
+                if user == 'guild':
+                    cursor.execute('insert into guild values(?, ?)',
+                                   (ssjson['guild_level'], points))
+                else:
+                    cursor.execute(
+                        'insert into contribution values(?,?)',
+                        (user, points))
+
+            conn.commit()
+            conn.close()
+
+    return errs
 
 
 db_dir, ss_dir = get_dirs()
-update_db(db_dir)
+errs = update_db(db_dir)
+errs = transfer_data(db_dir, ss_dir)
+
+if not errs:
+    print('It is now safe to remove the server settings directory')
