15f1622fcf45219214e49bbfe175eaf0c228d982
CJ Wong <christopher.jay.wong@gmail.com>
2019-02-11 22:06:46 -0800

    Move some constants from bot.py

--- constants/main/en_us.py
+++ constants/main/en_us.py
@@ -1,1 +1,45 @@
 BOSS_ALERT = '{} The following bosses will spawn within 15 minutes:\n\n'
+
+MSG_HELP = """
+Here are commands. Valid prefixes are `$` (dollar sign) and `Vaivora,<space>`,
+e.g. `$boss` or `Vaivora, help`
+
+```
+"Changelogs" commands
+    $unsubscribe
+    $subscribe
+
+* These functions are currently disabled.
+```
+```
+"Boss" commands
+    $boss [args ...]
+    $boss help
+
+* Use "$boss help" for more information.
+
+Examples:
+    $boss all list
+    $boss mineloader died 13:00 forest
+    $boss ml died 1:00p "forest of prayer"
+
+* More examples in "$boss help"
+```
+```
+"Settings" commands
+    $settings [args ...]
+    $settings help
+
+* Use "$settings help" for more information.
+
+Examples:
+    $settings set role auth @Leaders
+    $settings set role member @Members
+```
+```
+General
+    $help: prints this page in Direct Message
+```
+"""
+
+ROLE_BOSS = "boss"
