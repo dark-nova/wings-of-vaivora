a76798cad262367b870e942db47e17934523f282
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2019-02-26 16:41:55 -0800

    Move helper functions out of class

--- cogs/settings.py
+++ cogs/settings.py
@@ -17,6 +17,213 @@ from secrets import discord_user_id
 import constants.settings
 
 
+async def get_ids(self, ctx, mentions):
+        """
+        :func:`get_ids` filters out nonsense ints with actual id's.
+
+        Args:
+            ctx (discord.ext.commands.Context): context of the message
+            mentions: mentions to test for id's.
+
+        Returns:
+            list: of valid discord id's
+        """
+        if type(mentions) is int:
+            mentions = [mentions]
+
+        _mention = []
+        gids = [member.id for member in (ctx.guild.members
+                                         + ctx.guild.roles)]
+        rids = [member.id for member in ctx.guild.role]
+        if ctx.role_kind == constants.settings.ROLE_BOSS:
+            for mention in mentions:
+                # do not allow regular users for $boss
+                if mention in rids:
+                    _mention.append(mention)
+        else:
+            for mention in mentions:
+                if mention in gids:
+                    _mentions.append(mention)
+
+        return _mention
+
+
+async def channel_getter(self, ctx, kind):
+        """
+        :func:`channel_getter` does the work
+        for :func:`gc_boss` and :func:`gc_settings`.
+
+        Args:
+            ctx (discord.ext.commands.Context): context of the message
+            kind (str): the kind/type to use, i.e. subcommand invoked
+
+        Returns:
+            True always
+        """
+        vdb = vaivora.db.Database(ctx.guild.id)
+        channels = await vdb.get_channel(kind)
+
+        if not channels:
+            await ctx.send('{} {}'
+                           .format(ctx.author.mention,
+                                   constants.settings.FAIL_NO_CHANNELS
+                                   .format(kind)))
+        else:
+            channels = '\n'.join([str(ctx.guild.get_channel(channel))
+                                  for channel in channels])
+            channels = [ctx.guildchannel for channel in channels]
+            await ctx.send('{}\n\n{}'
+                           .format(ctx.author.mention,
+                                   constants.settings.SUCCESS_CHANNELS
+                                   .format(kind, channels)))
+        return True
+
+
+async def channel_setter(self, ctx, kind):
+        """
+        :func:`channel_setter` does the work
+        for :func:`sc_boss` and :func:`sc_settings`.
+
+        Args:
+            ctx (discord.ext.commands.Context): context of the message
+            kind (str): the kind/type to use, i.e. subcommand invoked
+
+        Returns:
+            True if successful; False otherwise
+        """
+        channels = []
+        for channel_mention in ctx.message.channel_mentions:
+            channels.append(str(channel_mention.id))
+        vdb = vaivora.db.Database(ctx.guild.id)
+        errs = []
+
+        for _channel in channels:
+            try:
+                await vdb.set_channel(kind, _channel)
+            except:
+                errs.append(_channel)
+
+        if not errs:
+            await ctx.send(constants.settings.SUCCESS
+                           .format(constants.settings.TABLE_CHANNEL,
+                                kind))
+        else:
+            await ctx.send(constants.settings.PARTIAL_SUCCESS
+                           .format(constants.settings.TABLE_CHANNEL,
+                                '\n'.join(errs)))
+        return True
+
+
+async def role_getter(ctx, mentions=None):
+        """
+        :func:`role_getter` handles the backend for
+        :func:`gr_member`, :func:`gr_auth`, and :func:`gr_boss`.
+
+        Args:
+            ctx (discord.ext.commands.Context): context of the message
+            mentions: (default: None) optional mentions that are only id's
+
+        Returns:
+            True if successful; False otherwise
+        """
+        _mentions = []
+
+        if ctx.message.role_mentions:
+            for mention in ctx.message.role_mentions:
+                _mentions.append(mention.id)
+
+        # do not allow regular users for $boss
+        if ctx.role_kind != constants.settings.ROLE_BOSS:
+            if ctx.message.mentions:
+                for mention in ctx.message.mentions:
+                    _mentions.append(mention.id)
+
+        else:
+            if not ctx.message.role_mentions and ctx.message.mentions:
+                await ctx.send('{} {}'
+                               .format(ctx.author.mention,
+                                       constants.settings.FAIL_NO_USER_BOSS))
+                return False
+
+        # uid mode; parse if they're actually id's and not nonsense
+        if mentions:
+            _mentions.append(await get_ids(ctx, mentions))
+
+        vdb = vaivora.db.Database(ctx.guild.id)
+        users = await vdb.get_users(ctx.role_kind, _mentions)
+
+        if users:
+            if _mentions:
+                users = [user for user in users if user in _mentions]
+            users = '\n'.join([str(ctx.guild.get_member(user))
+                               for user in users])
+            users = '```\n{}```'.format(users)
+            await ctx.send('{}\n\n{}'.format(
+                    ctx.author.mention,
+                    constants.settings.SUCCESS_ROLES.format(
+                        ctx.role_kind, users),
+                    constants.settings.NOTICE_ROLE))
+            return True
+        else:
+            await ctx.send('{} {}'.format(
+                    ctx.author.mention,
+                    constants.settings.FAIL_NO_ROLES.format(
+                        ctx.role_kind)))
+            return False
+
+
+async def role_setter(self, ctx, mentions=None):
+        """
+        :func:`role_setter` handles the backend for
+        :func:`sr_member`, :func:`sr_auth`, and :func:`sr_boss`.
+
+        Args:
+            ctx (discord.ext.commands.Context): context of the message
+            mentions: (default: None) optional mentions that are only id's
+
+        Returns:
+            True if successful; False otherwise
+        """
+        _mentions = []
+
+        if ctx.message.role_mentions:
+            for mention in ctx.message.role_mentions:
+                _mentions.append(mention.id)
+
+        # do not allow regular users for $boss
+        if ctx.role_kind != constants.settings.ROLE_BOSS:
+            if ctx.message.mentions:
+                for mention in ctx.message.mentions:
+                    _mentions.append(mention.id)
+
+        # uid mode; parse if they're actually id's and not nonsense
+        if mentions:
+            _mentions.append(await get_ids(ctx, mentions))
+
+        if not _mentions:
+            await ctx.send('{} {}'
+                           .format(ctx.author.mention,
+                                   constants.settings.FAIL_NO_MENTIONS))
+            return False
+
+        vdb = vaivora.db.Database(ctx.guild.id)
+        errs = await vdb.set_users(ctx.role_kind, _mentions)
+
+        if errs:
+            await ctx.send('{} {}'
+                           .format(ctx.author.mention,
+                                   constants.settings.PARTIAL_SUCCESS.format(
+                                        constants.settings.SETTING_SET,
+                                        '\n'.join(errs))))
+        else:
+            await ctx.send('{} {}'
+                           .format(ctx.author.mention,
+                                   constants.settings.SUCCESS_ROLES_UP.format(
+                                        ctx.role_kind)))
+
+        return True
+
+
 class SettingsCog:
 
     def __init__(self, bot):
@@ -117,7 +324,7 @@ class SettingsCog:
         Returns:
             True if successful; False otherwise
         """
-        return await self.channel_setter(ctx, ctx.channel_kind)
+        return await channel_setter(ctx, ctx.channel_kind)
 
     @s_channel.command(name='boss')
     @checks.only_in_guild()
@@ -134,41 +341,7 @@ class SettingsCog:
         Returns:
             True if successful; False otherwise
         """
-        return await self.channel_setter(ctx, ctx.channel_kind)
-
-    async def channel_setter(self, ctx, kind):
-        """
-        :func:`channel_setter` does the work
-        for :func:`sc_boss` and :func:`sc_settings`.
-
-        Args:
-            ctx (discord.ext.commands.Context): context of the message
-            kind (str): the kind/type to use, i.e. subcommand invoked
-
-        Returns:
-            True if successful; False otherwise
-        """
-        channels = []
-        for channel_mention in ctx.message.channel_mentions:
-            channels.append(str(channel_mention.id))
-        vdb = vaivora.db.Database(ctx.guild.id)
-        errs = []
-
-        for _channel in channels:
-            try:
-                await vdb.set_channel(kind, _channel)
-            except:
-                errs.append(_channel)
-
-        if not errs:
-            await ctx.send(constants.settings.SUCCESS
-                           .format(constants.settings.TABLE_CHANNEL,
-                                kind))
-        else:
-            await ctx.send(constants.settings.PARTIAL_SUCCESS
-                           .format(constants.settings.TABLE_CHANNEL,
-                                '\n'.join(errs)))
-        return True
+        return await channel_setter(ctx, ctx.channel_kind)
 
     @_set.group(name='role', aliases=['roles', 'user', 'users'])
     @checks.only_in_guild()
@@ -202,7 +375,7 @@ class SettingsCog:
         Returns:
             True if successful; False otherwise      
         """
-        return await self.role_setter(ctx, mentions)
+        return await role_setter(ctx, mentions)
 
     @s_role.group(name='authorized', aliases=['auth'])
     @checks.only_in_guild()
@@ -218,7 +391,7 @@ class SettingsCog:
         Returns:
             True if successful; False otherwise      
         """
-        return await self.role_setter(ctx, mentions)
+        return await role_setter(ctx, mentions)
 
     @s_role.group(name='boss')
     @checks.only_in_guild()
@@ -234,58 +407,7 @@ class SettingsCog:
         Returns:
             True if successful; False otherwise      
         """
-        return await self.role_setter(ctx, mentions)
-
-    async def role_setter(self, ctx, mentions=None):
-        """
-        :func:`role_setter` handles the backend for
-        :func:`sr_member`, :func:`sr_auth`, and :func:`sr_boss`.
-
-        Args:
-            ctx (discord.ext.commands.Context): context of the message
-            mentions: (default: None) optional mentions that are only id's
-
-        Returns:
-            True if successful; False otherwise
-        """
-        _mentions = []
-
-        if ctx.message.role_mentions:
-            for mention in ctx.message.role_mentions:
-                _mentions.append(mention.id)
-
-        # do not allow regular users for $boss
-        if ctx.role_kind != constants.settings.ROLE_BOSS:
-            if ctx.message.mentions:
-                for mention in ctx.message.mentions:
-                    _mentions.append(mention.id)
-
-        # uid mode; parse if they're actually id's and not nonsense
-        if mentions:
-            _mentions.append(await self.get_ids(ctx, mentions))
-
-        if not _mentions:
-            await ctx.send('{} {}'
-                           .format(ctx.author.mention,
-                                   constants.settings.FAIL_NO_MENTIONS))
-            return False
-
-        vdb = vaivora.db.Database(ctx.guild.id)
-        errs = await vdb.set_users(ctx.role_kind, _mentions)
-
-        if errs:
-            await ctx.send('{} {}'
-                           .format(ctx.author.mention,
-                                   constants.settings.PARTIAL_SUCCESS.format(
-                                        constants.settings.SETTING_SET,
-                                        '\n'.join(errs))))
-        else:
-            await ctx.send('{} {}'
-                           .format(ctx.author.mention,
-                                   constants.settings.SUCCESS_ROLES_UP.format(
-                                        ctx.role_kind)))
-
-        return True
+        return await role_setter(ctx, mentions)
 
     @_set.group(name='talt', aliases=['points', 'pt', 'pts',
                                       'contrib', 'contribs',
@@ -343,7 +465,7 @@ class SettingsCog:
                                    .FAIL_TOO_MANY_MENTIONS))
             return False
         elif member:
-            mention = (await self.get_ids(ctx, member))[0]
+            mention = (await get_ids(ctx, member))[0]
 
         if not member:
             member = ctx.author.id
@@ -399,7 +521,7 @@ class SettingsCog:
         Returns:
             True always
         """
-        return await self.channel_getter(ctx, ctx.channel_kind)
+        return await channel_getter(ctx, ctx.channel_kind)
 
     @g_channel.command(name='boss')
     @checks.only_in_guild()
@@ -414,37 +536,7 @@ class SettingsCog:
         Returns:
             True always
         """
-        return await self.channel_getter(ctx, ctx.channel_kind)
-
-    async def channel_getter(self, ctx, kind):
-        """
-        :func:`channel_getter` does the work
-        for :func:`gc_boss` and :func:`gc_settings`.
-
-        Args:
-            ctx (discord.ext.commands.Context): context of the message
-            kind (str): the kind/type to use, i.e. subcommand invoked
-
-        Returns:
-            True always
-        """
-        vdb = vaivora.db.Database(ctx.guild.id)
-        channels = await vdb.get_channel(kind)
-
-        if not channels:
-            await ctx.send('{} {}'
-                           .format(ctx.author.mention,
-                                   constants.settings.FAIL_NO_CHANNELS
-                                   .format(kind)))
-        else:
-            channels = '\n'.join([str(ctx.guild.get_channel(channel))
-                                  for channel in channels])
-            channels = [ctx.guildchannel for channel in channels]
-            await ctx.send('{}\n\n{}'
-                           .format(ctx.author.mention,
-                                   constants.settings.SUCCESS_CHANNELS
-                                   .format(kind, channels)))
-        return True
+        return await channel_getter(ctx, ctx.channel_kind)
 
     @_get.group(name='role', aliases=['roles', 'user', 'users'])
     @checks.only_in_guild()
@@ -476,7 +568,7 @@ class SettingsCog:
         Returns:
             True if successful; False otherwise
         """
-        return await self.role_getter(ctx, mentions)
+        return await role_getter(ctx, mentions)
 
     @g_role.group(name='authorized', aliases=['auth'])
     @checks.only_in_guild()
@@ -491,7 +583,7 @@ class SettingsCog:
         Returns:
             True if successful; False otherwise
         """
-        return await self.role_getter(ctx, mentions)
+        return await role_getter(ctx, mentions)
 
     @g_role.group(name='boss')
     @checks.only_in_guild()
@@ -506,64 +598,7 @@ class SettingsCog:
         Returns:
             True if successful; False otherwise
         """
-        return await self.role_getter(ctx, mentions)
-
-    async def role_getter(self, ctx, mentions=None):
-        """
-        :func:`role_getter` handles the backend for
-        :func:`gr_member`, :func:`gr_auth`, and :func:`gr_boss`.
-
-        Args:
-            ctx (discord.ext.commands.Context): context of the message
-            mentions: (default: None) optional mentions that are only id's
-
-        Returns:
-            True if successful; False otherwise
-        """
-        _mentions = []
-
-        if ctx.message.role_mentions:
-            for mention in ctx.message.role_mentions:
-                _mentions.append(mention.id)
-
-        # do not allow regular users for $boss
-        if ctx.role_kind != constants.settings.ROLE_BOSS:
-            if ctx.message.mentions:
-                for mention in ctx.message.mentions:
-                    _mentions.append(mention.id)
-
-        else:
-            if not ctx.message.role_mentions and ctx.message.mentions:
-                await ctx.send('{} {}'
-                               .format(ctx.author.mention,
-                                       constants.settings.FAIL_NO_USER_BOSS))
-                return False
-
-        # uid mode; parse if they're actually id's and not nonsense
-        if mentions:
-            _mentions.append(await self.get_ids(ctx, mentions))
-
-        vdb = vaivora.db.Database(ctx.guild.id)
-        users = await vdb.get_users(ctx.role_kind, _mentions)
-
-        if users:
-            if _mentions:
-                users = [user for user in users if user in _mentions]
-            users = '\n'.join([str(ctx.guild.get_member(user))
-                               for user in users])
-            users = '```\n{}```'.format(users)
-            await ctx.send('{}\n\n{}'.format(
-                    ctx.author.mention,
-                    constants.settings.SUCCESS_ROLES.format(
-                        ctx.role_kind, users),
-                    constants.settings.NOTICE_ROLE))
-            return True
-        else:
-            await ctx.send('{} {}'.format(
-                    ctx.author.mention,
-                    constants.settings.FAIL_NO_ROLES.format(
-                        ctx.role_kind)))
-            return False
+        return await role_getter(ctx, mentions)
 
     @_get.group(name='talt', aliases=['points', 'pt', 'pts',
                                       'contrib', 'contribs',
@@ -574,35 +609,7 @@ class SettingsCog:
     async def g_talt(self, ctx, mentions: Optional[int] = None):
         pass
 
-    async def get_ids(self, ctx, mentions):
-        """
-        :func:`get_ids` filters out nonsense ints with actual id's.
-
-        Args:
-            ctx (discord.ext.commands.Context): context of the message
-            mentions: mentions to test for id's.
-
-        Returns:
-            list: of valid discord id's
-        """
-        if type(mentions) is int:
-            mentions = [mentions]
-
-        _mention = []
-        gids = [member.id for member in (ctx.guild.members
-                                         + ctx.guild.roles)]
-        rids = [member.id for member in ctx.guild.role]
-        if ctx.role_kind == constants.settings.ROLE_BOSS:
-            for mention in mentions:
-                # do not allow regular users for $boss
-                if mention in rids:
-                    _mention.append(mention)
-        else:
-            for mention in mentions:
-                if mention in gids:
-                    _mentions.append(mention)
-
-        return _mention
+    
 
 def setup(bot):
     bot.add_cog(SettingsCog(bot))
