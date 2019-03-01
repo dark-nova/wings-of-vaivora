28d8153c256e8394930290d2ae3840f5c392682b
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2019-03-01 10:49:01 -0800

    Add beginnings of calculating abrasives exp

--- cogs/gems.py
+++ cogs/gems.py
@@ -4,6 +4,9 @@ from discord.ext import commands
 import checks
 
 
+abrasives = [0, 0, 300, 1200, 3900, 14700, 57900, 230700, 1094700, 5414700]
+
+
 class GemsCog:
 
     def __init__(self, bot):
@@ -11,6 +14,13 @@ class GemsCog:
 
     @commands.group()
     async def gems(self, ctx)
+        pass
+
+    @gems.command()
+    async def exp(self, ctx, abrasives: commands.Greedy[str]):
+        """
+        :func:`exp` calculates the exp obtained from abrasives.
+        """
         pass
 
 def setup(bot):
