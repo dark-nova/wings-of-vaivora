fe11af9d9f608f306fc8749bf4e858b0f1f60434
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2019-04-15 21:05:53 +0000

    Add messages for commands

--- constants/events.py
+++ constants/events.py
@@ -1,4 +1,11 @@
 MODULE_NAME = 'events'
 COMMAND = '$' + MODULE_NAME
 
+SUCCESS_EVENT_ADD = 'Your custom event **{}** was added with an ending date of **{}**.'
+
+FAIL_EVENT_ADD = 'Your custom event could not be added. Verify if it already exists.\nYou may also be at the limit of 15 events.'
+FAIL_EVENT_NAME = 'You cannot add this event as it has the same name as a permanent event.'
+FAIL_EVENT_NAME_INVALID = 'Your custom event name is invalid.'
+FAIL_EVENT_UPDATE = 'You cannot update nonexistent events.'
+
 FAIL_INVALID_DATE = 'Your date was invalid.'
