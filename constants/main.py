8f84c316fa75c77c7fbb2a2bda94973fddb46fdf
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2019-04-23 09:11:51 -0700

    Simplify help text, remove unused vars

--- constants/main.py
+++ constants/main.py
@@ -7,57 +7,15 @@ Here are commands. Valid prefixes are `$` (dollar sign) and `Vaivora,<space>`,
 e.g. `$boss` or `Vaivora, help`
 
 `$boss` commands
-```
-$boss [args ...]
-$boss help
-
-* Use "$boss help" for more information.
-
-Examples:
-    $boss all list
-    $boss mineloader died 13:00 forest
-    $boss ml died 1:00p "forest of prayer"
-```
 More info: <https://github.com/dark-nova/wings-of-vaivora/blob/master/docs/BOSS.md>
 
 `$settings` commands
-```
-$settings [args ...]
-$settings help
-
-* Use "$settings help" for more information.
-
-Examples:
-    $settings set role auth @Leaders
-    $settings set role member @Members
-```
 More info: <https://github.com/dark-nova/wings-of-vaivora/blob/master/docs/SETTINGS.md>
 
 `$gems` commands
-```
-$settings [args ...]
-$settings help
-
-* Use "$gems help" for more information.
-
-Examples:
-    $gems exp 1x1 1x2
-    $gems gem2lv 1 0 2
-```
 More info: <https://github.com/dark-nova/wings-of-vaivora/blob/master/docs/GEMS.md>
 
 `$offset` commands
-```
-$offset [args ...]
-$offset help
-
-* Use "$offset help" for more information.
-
-Examples:
-    $offset list
-    $offset set offset 1
-    $offset set tz 0
-```
 More info: <https://github.com/dark-nova/wings-of-vaivora/blob/master/docs/OFFSET.md>
 
 General
@@ -73,10 +31,6 @@ Please read the following before continuing.
 """ + disclaimer + """
 Anyone may contribute to this bot's development: https://github.com/dark-nova/wings-of-vaivora
 """
-
-ROLE_SETTINGS = "settings"
-
-HELP = 'help'
 
 FAIL_CANT_DM = '**{}** is not available in Direct Messages.'
 FAIL_TOO_FEW_ARGS = '{} Too few or many arguments for `{}`.\n\nUsage: `{}`'
